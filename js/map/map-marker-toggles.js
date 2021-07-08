fec19e662d499d77cacf352d774d051c41fb6a65
markoco14 <76949608+markoco14@users.noreply.github.com>
2021-07-08 14:03:48 +0700

    Add files via upload

--- js/map/map-marker-toggles.js
+++ js/map/map-marker-toggles.js
@@ -3,18 +3,18 @@
 
 //create references to all toggle buttons
 const footprintToggleButton = document.getElementById('footprint-button');
-const hideDisinfectionsButton = document.getElementById('hide-disinfections');
-const showDisinfectionsButton = document.getElementById('show-disinfections');
-const hideChineseHospitalsButton = document.getElementById('hide-chinese-hospitals');
-const showChineseHospitalsButton = document.getElementById('show-chinese-hospitals');
-const hideEnglishHopsitalsButton = document.getElementById('hide-english-hospitals');
-const showEnglishHospitalsButton = document.getElementById('show-english-hospitals');
+const disinfectionToggleButton = document.getElementById('disinfection-button');
+const chineseHospitalToggleButton = document.getElementById('chinese-hospitals-button');
+const englishHospitalToggleButton = document.getElementById('english-hospitals-button');
+
 //need referenece to chinese hospital buttons
 //need referenece to english hospital buttons
 
 //create variables to track active/inactive state
 let footprintMarkersActive = true;
 let disinfectionMarkersActive = true;
+let chineseMarkersActive = true;
+let englishMarkersActive = true;
 
 
 /* toggle functions
@@ -24,34 +24,77 @@ beyond the normal show/hide marker functions
 */
 
 let toggleFootprints = function() {
-	//check if the footprints are already displayed
-	//if not:
 	if (!footprintMarkersActive) {
-		/*for (i = 0; i < markerInfo.length; i++) {
-			//noooo I need to make a function
-			//that intializes that array
-			//but does not show the markers
-			//watch I'm about to continuously add markers to
-			//the array
-			markers[i].setMap(map);
-		}*/
 		showFootprintMarkers()
 		footprintToggleButton.textContent = "Hide footprints";
 		footprintMarkersActive = true;
-		//console.log(footprintMarkers);
+		footprintToggleButton.classList.add("pressed")
+
 	} else {
-		/*for (let i = 0; i < markers.length; i++) {
-		    markers[i].setMap(null);
-		}*/
 		hideFootprintMarkers()
 		footprintToggleButton.textContent = "Show footprints";
 		footprintMarkersActive = false;
-		//console.log(footprintMarkers);
+		footprintToggleButton.classList.remove("pressed")
+	}
+}
+
+let toggleDisinfections = function () {
+	if (!disinfectionMarkersActive) {
+		showDisinfectionMarkers()
+		disinfectionToggleButton.textContent = "Hide disinfections";
+		disinfectionMarkersActive = true;
+		disinfectionToggleButton.classList.add("pressed")
+	} else {
+		hideDisinfectionMarkers()
+		disinfectionToggleButton.textContent = "Show disinfections";
+		disinfectionMarkersActive = false;
+		disinfectionToggleButton.classList.remove("pressed")
+	}
+}
+
+let toggleChineseHospitals = function() {
+	if (!chineseMarkersActive) {
+		showChineseHospitalMarkers()
+		chineseHospitalToggleButton.textContent = "Hide Chinese hopsitals"
+		chineseMarkersActive = true;
+		chineseHospitalToggleButton.classList.add("pressed")
+	} else {
+		hideChineseHospitalMarkers()
+		chineseHospitalToggleButton.textContent = "Show Chinese hopsitals"
+		chineseMarkersActive = false;
+		chineseHospitalToggleButton.classList.remove("pressed")
+	}
+}
+
+let toggleEnglishHospitals = function() {
+	if (!englishMarkersActive) {
+		showEnglishHospitalMarkers()
+		englishHospitalToggleButton.textContent = "Hide English hopsitals"
+		englishMarkersActive = true;
+		englishHospitalToggleButton.classList.add("pressed")
+	} else {
+		hideEnglishHospitalMarkers()
+		englishHospitalToggleButton.textContent = "Show English hopsitals"
+		englishMarkersActive = false;
+		englishHospitalToggleButton.classList.remove("pressed")
 	}
 }
 
 // create all toggle button listeners
 footprintToggleButton.addEventListener('click', toggleFootprints);
+disinfectionToggleButton.addEventListener('click', toggleDisinfections)
+chineseHospitalToggleButton.addEventListener('click', toggleChineseHospitals);
+englishHospitalToggleButton.addEventListener('click', toggleEnglishHospitals);
+
+
+
+/* test before depracating 
+const hideDisinfectionsButton = document.getElementById('hide-disinfections');
+const showDisinfectionsButton = document.getElementById('show-disinfections');
+const hideChineseHospitalsButton = document.getElementById('hide-chinese-hospitals');
+const showChineseHospitalsButton = document.getElementById('show-chinese-hospitals');
+const hideEnglishHopsitalsButton = document.getElementById('hide-english-hospitals');
+const showEnglishHospitalsButton = document.getElementById('show-english-hospitals');
 
 hideDisinfectionsButton.addEventListener('click', hideDisinfectionMarkers);
 showDisinfectionsButton.addEventListener('click', showDisinfectionMarkers);
@@ -60,4 +103,6 @@ hideChineseHospitalsButton.addEventListener('click', hideChineseHospitalMarkers)
 showChineseHospitalsButton.addEventListener('click', showChineseHospitalMarkers);
 
 hideEnglishHopsitalsButton.addEventListener('click', hideEnglishHospitalMarkers);
-showEnglishHospitalsButton.addEventListener('click', showEnglishHospitalMarkers)
+showEnglishHospitalsButton.addEventListener('click', showEnglishHospitalMarkers);
+
+*/
