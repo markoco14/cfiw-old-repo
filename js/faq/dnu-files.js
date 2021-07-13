d59c7e07bb55a0c5767c4a667c454860d4a8a0dc
markoco14 <76949608+markoco14@users.noreply.github.com>
2021-07-13 18:05:33 +0700

    Add files via upload

---
+++ js/faq/dnu-files.js
@@ -0,0 +1,6 @@
+//create do not use punctuation array
+let dnuPunctuation = ["?", ".", ",", "/", "<", ">", "'", '"', ";", ":", "[", "]", "{", "}", "|", "`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "\\"] ;
+
+//create do not use word array
+let dnuWords = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "you", "he", "she", "it", "we", "they", "am", "are", "re", "can", "can't", "may", "will", "ll", "won't", "could", "couldn't", "would", "wouldn't", "should", "shouldn't", "shall", "has", "hasn't", "have", "ve", "haven't", "maybe", "but", "and", "or", "go", "goes", "the", "an", "for", "as", "on", "in", "at", "next", "by", "to", "give", "let", "take", "me", "her", "his", "him", "hers", "their", "theirs", "its", "us", "our", "ours", "mine", "your", "yours", "gonna", "want", "like", "love", "wanna", "gonna", "lot", "many", "much", "very", "good", "bad", "still", "out", "next", "first", "last", "previous", "before", "after", "covid", "taichung", "taiwan", "covid19"];
+
